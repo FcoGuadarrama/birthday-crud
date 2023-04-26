75175ab202c0eb5182df26424781d79dd200341c
Fco Guadarrama <ingfranciscoguadarrama@gmail.com>
2023-04-26 01:40:16 -0600

    mejora estilos de tabla

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,49 +8,41 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <div class="card">
-                        <div class="card-header">Users</div>
-
-                        <div class="card-body">
-
-                            <table class="table table-bordered">
-                                <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Age</th>
-                                    <th>Actions</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach ($users as $user)
-                                    <tr>
-                                        <td>{{ $user->id }}</td>
-                                        <td>{{ $user->name }}</td>
-                                        <td>{{ $user->email }}</td>
-                                        <td>{{ \Carbon\Carbon::createFromDate($user->birthday)->age }}</td>
-                                        <td>
-                                            <a href="{{ route('users.edit', $user->id) }}"
-                                               class="btn btn-sm btn-primary">Edit</a>
-                                            <form action="{{ route('users.destroy', $user->id) }}" method="post"
-                                                  class="d-inline">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-sm btn-danger"
-                                                        onclick="return confirm('¿Are you sure?')">
-                                                    Delete
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+                <table class="table w-full border-collapse border border-gray-400">
+                    <thead>
+                    <tr>
+                        <th class="p-2 border border-gray-400">ID</th>
+                        <th class="p-2 border border-gray-400">Name</th>
+                        <th class="p-2 border border-gray-400">Email</th>
+                        <th class="p-2 border border-gray-400">Age</th>
+                        <th class="p-2 border border-gray-400">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td class="p-2 border border-gray-400">{{ $user->id }}</td>
+                            <td class="p-2 border border-gray-400">{{ $user->name }}</td>
+                            <td class="p-2 border border-gray-400">{{ $user->email }}</td>
+                            <td class="p-2 border border-gray-400">{{ \Carbon\Carbon::createFromDate($user->birthday)->age }}</td>
+                            <td class="p-2 border border-gray-400">
+                                <div class="text-center">
+                                    <a href="{{ route('users.edit', $user->id) }}"
+                                       class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</a>
+                                    <form action="{{ route('users.destroy', $user->id) }}" method="post" class="inline-block p-4 ">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                                onclick="return confirm('¿Are you sure?')">
+                                            Delete
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
